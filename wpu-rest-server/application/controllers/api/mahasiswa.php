d2d705f428244e88bcb0b3c17731b5aa5f249cb9
PutriMutiara <ptrmtr08@gmail.com>
2025-05-28 15:39:56 +0700

    autentikasi vidio 8 done

--- wpu-rest-server/application/controllers/api/mahasiswa.php
+++ wpu-rest-server/application/controllers/api/mahasiswa.php
@@ -13,6 +13,7 @@ class mahasiswa extends REST_Controller {
         parent::__construct();
         $this->load->model('Mahasiswa_model', 'mahasiswa');
 
+         $this->methods['index_get']['limit'] = 100 ;
     }
 
     public function index_get() {
