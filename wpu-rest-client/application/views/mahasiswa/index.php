59915096cab8e1d1d889c219acc6008b88cbd152
PutriMutiara <ptrmtr08@gmail.com>
2025-05-29 14:49:36 +0700

    menambahkan portofolio

--- wpu-rest-client/application/views/mahasiswa/index.php
+++ wpu-rest-client/application/views/mahasiswa/index.php
@@ -22,19 +22,6 @@
 
     <div class="row mt-3">
         <div class="col-md-6">
-            <form action="" method="post">
-                <div class="input-group">
-                    <input type="text" class="form-control" placeholder="Cari data mahasiswa.." name="keyword">
-                    <div class="input-group-append">
-                        <button class="btn btn-primary" type="submit">Cari</button>
-                    </div>
-                </div>
-            </form>
-        </div>
-    </div>
-
-    <div class="row mt-3">
-        <div class="col-md-6">
             <h3>Daftar Mahasiswa</h3>
             <?php if (empty($mahasiswa)) : ?>
                 <div class="alert alert-danger" role="alert">
